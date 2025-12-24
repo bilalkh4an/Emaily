87b09c6b67cdb9ff7bfead1777831458d4f674fb
Bilal Khan <bilal.kh4an@gmail.com>
2025-12-24 12:13:48 +0500

    Update folders

---
+++ 01-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Notes from './assets/Components/Notes'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Notes />
+    </>
+  )
+}
+
+export default App
