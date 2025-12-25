056cdaecda9d32f48de9b12f06bb10d6b045b96b
Bilal Khan <bilal.kh4an@gmail.com>
2025-12-25 12:06:26 +0500

    registration UI updated 01

--- HMS/src/App.jsx
+++ HMS/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 import PatientRegistration from './Components/PatientRegistration'
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <PatientRegistration />
+      <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
     </>
   )
 }
--- HMS/src/Components/PatientRegistration.jsx
+++ HMS/src/Components/PatientRegistration.jsx
@@ -1,7 +1,78 @@
+import { User, CalendarDays,VenusAndMars, Mail, Phone } from "lucide-react";
 const PatientRegistration = () => {
 
     return (
-        <h1>Hellow</h1>
+        <div className="bg-[url(./assets/background2.png)] bg-cover bg-center bg-no-repeat">        
+       <div className="h-screen px-10 py-10 shadow-2xl">          
+            <div className="w-6/12 px-10 py-10 ml-auto shadow-2xl shadow-black/50 rounded-2xl">
+                <h1 className="text-center py-2 font-medium text-2xl">Pattient Registration</h1>
+                <p className="text-center text-gray-600">Step 1:4 Personal Information</p>
+                <div className="flex items-center justify-center w-full my-8">
+  {/* Step 1 */}
+  <div className="flex items-center">
+    <div className="w-8 h-8 rounded-full bg-indigo-600 text-white flex items-center justify-center font-semibold">
+      1
+    </div>
+    <span className="ml-2 text-sm font-medium text-indigo-600">
+      Personal Info
+    </span>
+  </div>
+
+  {/* Line */}
+  <div className="w-24 h-[2px] bg-indigo-300 mx-4"></div>
+
+  {/* Step 2 */}
+  <div className="flex items-center">
+    <div className="w-8 h-8 rounded-full bg-gray-300 text-gray-600 flex items-center justify-center font-semibold">
+      2
+    </div>
+    <span className="ml-2 text-sm text-gray-500">
+      Medical Info
+    </span>
+  </div>
+
+  {/* Line */}
+  <div className="w-24 h-[2px] bg-gray-300 mx-4"></div>
+
+  {/* Step 3 */}
+  <div className="flex items-center">
+    <div className="w-8 h-8 rounded-full bg-gray-300 text-gray-600 flex items-center justify-center font-semibold">
+      3
+    </div>
+    <span className="ml-2 text-sm text-gray-500">
+      Confirmation
+    </span>
+  </div>
+</div>
+
+                <form className="">                
+  
+                <div className="flex items-center gap-2 text-gray-700 mx-2 my-2">
+                <User size={18} className="text-blue-600" />
+                <span className="text-sm font-medium">Patient Name</span>
+                </div>
+                <input className="w-full border border-gray-600 rounded  px-5 py-2 mx-2 my-1" placeholder="Patient Name" />
+
+                <div className="flex items-center gap-2 text-gray-700 mx-2 my-2">
+                <CalendarDays  size={18} className="text-blue-600" />
+                <span className="text-sm font-medium">Date of Birth</span>
+                </div>              
+                <input className="w-full border rounded  px-5 py-2 mx-2 my-1" placeholder="Date of Birth" />
+
+
+                
+                <div className="flex items-center gap-2 text-gray-700 mx-2 my-2">
+                <VenusAndMars  size={18} className="text-blue-600" />
+                <span className="text-sm font-medium">Gender</span>
+                </div>
+                <input className="w-full border rounded  px-5 py-2 mx-2 my-1" placeholder="Gender" />
+
+                    <button className="bg-black text-white py-2 px-20 mx-20 my-5 rounded-2xl">Back</button>
+                    <button className="bg-black text-white py-2 px-20 rounded-2xl">Next</button>
+                </form>    
+            </div>            
+        </div>
+        </div>
     )
 }
 export default PatientRegistration
